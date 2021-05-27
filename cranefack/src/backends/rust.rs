e4ae5e54b0d462c55d3dfbee4a923a06baca2ca4
Volker Ströbel <volkerstroebel@mysurdity.de>
2021-05-27 11:30:16 +0200

    Remove different optimizations from rust backend

--- cranefack/src/backends/rust.rs
+++ cranefack/src/backends/rust.rs
@@ -3,16 +3,12 @@ use std::fmt::Write;
 use crate::parser::{Program, Op, OpType};
 use std::error::Error;
 
-pub fn build_file(program: &Program, opt_mode: &str) -> String {
+pub fn build_file(program: &Program) -> String {
     let mut code = "".to_owned();
 
     print_ops(&mut code, &program.ops).expect("No io error");
 
-    match opt_mode {
-        "2" => include_str!("rust_o2.tpl").replace("{{CODE}}", &code),
-        "3" => include_str!("rust_o3.tpl").replace("{{CODE}}", &code),
-        _ => include_str!("rust.tpl").replace("{{CODE}}", &code)
-    }
+    include_str!("rust.tpl").replace("{{CODE}}", &code)
 }
 
 fn print_ops(out: &mut String, ops: &[Op]) -> Result<(), Box<dyn Error>> {
